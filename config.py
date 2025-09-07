7a5d2a0b0373d676aec974d5b4f4f959e71b6f5a
Yury MonZon <yurymonzon@gmail.com>
2025-09-07 16:03:04 +0200

    Default config

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+PROXMOX_HOST = "192.168.1.123"
+PROXMOX_USER = "root@pam"
+PROXMOX_TOKEN_ID = "Token_Name"
+PROXMOX_TOKEN_SECRET = "Token_Secret"
+VERIFY_SSL = False
+
+# List of VMs to manage
+VM_IDS = [100, 101, 102, 103]
+
+# If True, show confirmation dialog before sending actions
+SHY = True
+
